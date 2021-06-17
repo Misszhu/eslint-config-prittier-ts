eda3cd236e048ea5b6335867fe22de3f8980b302
zhuhuiting <zhuhuiting@joyy.sg>
2021-06-17 11:45:24 +0800

    feat: 修改eslint规则

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
+    parser: '@typescript-eslint/parser', //  一个 ESLint 特定的解析器，它利用typescript-estree并被设计用来替代 ESLint 的默认解析器，espree.
     extends: [
+      'plugin:react/recommended',
+      'plugin:react-hooks/recommended',
       'plugin:@typescript-eslint/recommended',
       'prettier/@typescript-eslint',
       'plugin:prettier/recommended',
-      'airbnb-typescript'
-    ],
+    ], //使用推荐的React代码检测规范
+    // 插件名
     plugins: ['@typescript-eslint'],
     // 指定要启用的环境
     env: {
       browser: true,
       node: true,
     },
-    parserOptions: {    
+    settings: {
+      //自动发现React的版本，从而进行规范react代码
+      react: {
+        pragma: 'React',
+        version: 'detect',
+      },
+    },
+    parserOptions: {
       //指定ESLint可以解析JSX语法
       ecmaVersion: 2020,
       sourceType: 'module',
@@ -22,6 +31,7 @@ module.exports = {
     },
     rules: {
       '@typescript-eslint/no-var-requires': 0,
+      'react/react-in-jsx-scope': 0,
       '@typescript-eslint/explicit-function-return-type': 0,
       '@typescript-eslint/no-empty-function': 0, // 禁止出现空函数
       '@typescript-eslint/no-explicit-any': 0,
@@ -29,6 +39,9 @@ module.exports = {
       '@typescript-eslint/no-use-before-define': 0, // 禁止在变量定义之前使用它们
       '@typescript-eslint/class-name-casing': 0,
       '@typescript-eslint/triple-slash-reference': 0,
+      '@typescript-eslint/ban-types': 0,
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
       'prettier/prettier': [
         'error',
         {
@@ -41,7 +54,7 @@ module.exports = {
           arrowParens: 'avoid',
           insertPragma: false,
           tabWidth: 2,
-          useTabs: false, 
+          useTabs: false,
           endOfLine: 'auto',
         },
       ],
